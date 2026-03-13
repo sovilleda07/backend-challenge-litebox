17077e12c3d2d8fc27ba368b3a7878028322a062
sovilleda07 <sovilleda07@gmail.com>
2026-03-12 22:45:03 -0600

    feat: enable CORS for API access

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,11 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,POST',
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
